9f26d8c7cc46e13a825e0dd60884d1d11cb74465
Luka Gurdulic <gurdulicluka@gmail.com>
2023-04-19 17:58:08 +0200

    Add local font files, configure tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      poppins: ["poppins", "sans-serif"],
+      danley: ["danley", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
