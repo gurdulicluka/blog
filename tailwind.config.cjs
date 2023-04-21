f545dfe649a10599d89ee25401f37927275a91cc
Luka Gurdulic <gurdulicluka@gmail.com>
2023-04-21 17:28:21 +0200

    Add Navbar component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "0.5rem",
+        sm: "1rem",
+        md: "2rem",
+        "2xl": "4rem",
+      },
+    },
     fontFamily: {
       poppins: ["poppins", "sans-serif"],
       danley: ["danley", "sans-serif"],
