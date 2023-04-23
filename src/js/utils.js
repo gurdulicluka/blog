2d09438ec14f96e935a50345306c292573cc5ef4
Luka Gurdulic <gurdulicluka@gmail.com>
2023-04-23 18:51:33 +0200

    Add missing helper script

---
+++ src/js/utils.js
@@ -0,0 +1,16 @@
+export function slugify(text) {
+  return text
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w-]+/g, "")
+    .replace(/--+/g, "-")
+    .replace(/^-+/, "")
+    .replace(/-+$/, "");
+}
+
+export function formatDate(dateString) {
+  const date = new Date(dateString);
+  const options = { day: "numeric", month: "long", year: "numeric" };
+  return date.toLocaleDateString("en-US", options);
+}
