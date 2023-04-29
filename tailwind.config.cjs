362a87cf742f15f3f861de5c2e8e89790fae6847
Luka Gurdulic <gurdulicluka@gmail.com>
2023-04-29 18:18:51 +0200

    Add daisy start config for themes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#10B981",
+          "base-100": "#F9FAFB",
+          neutral: "#171717",
+        },
+      },
+      {
+        dark: {
+          primary: "#047857",
+          "base-100": "#1F2937",
+          neutral: "#171717",
+        },
+      },
+    ],
+  },
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          100: { opacity: "100" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 250ms ease-in 1",
+      },
+    },
     container: {
       center: true,
       padding: {
@@ -16,5 +43,5 @@ module.exports = {
       poppins: ["Poppins", "sans-serif"],
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
