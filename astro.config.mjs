b06dd121012a16118f42fd1adf7b93c664619c77
Luka Gurdulic <phallondev@gmail.com>
2023-05-04 17:51:54 +0200

    Add Partytown package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import alpinejs from "@astrojs/alpinejs";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +10,5 @@ export default defineConfig({
     config: {
       applyBaseStyles: false
     }
-  }), alpinejs()]
+  }), alpinejs(), partytown()]
 });
