16f6e375663aa3caba6d7e70ba4d7f62da1d4271
Luka Gurdulic <phallondev@gmail.com>
2023-06-06 10:51:49 +0200

    Add sitemap integration
    Add SEO (Sitemap, rich text, open graph)
    
    
    Delete Todo.txt
    Styling and font change to code blocks, more indented
    
    
    Add rehype-prettier-code package, style code block and highlighting
    
    
    Todo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,22 +3,63 @@ import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 import partytown from "@astrojs/partytown";
 import image from "@astrojs/image";
-
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import rehypePrettyCode from "rehype-pretty-code";
+
+const prettyCodeOptions = {
+  theme: "github-dark",
+  onVisitLine(node) {
+    if (node.children.length === 0) {
+      node.children = [
+        {
+          type: "text",
+          value: " ",
+        },
+      ];
+    }
+  },
+  onVisitHighlightedLine(node) {
+    node.properties.className.push("highlighted");
+  },
+  onVisitHighlightedWord(node, id) {
+    node.properties.className = ["word"];
+    if (id) {
+      // If the word spans across syntax boundaries (e.g. punctuation), remove
+      // colors from the child nodes.
+      if (node.properties["data-rehype-pretty-code-wrapper"]) {
+        node.children.forEach((childNode) => {
+          childNode.properties.style = "";
+        });
+      }
+
+      node.properties.style = "";
+      node.properties["data-word-id"] = id;
+    }
+  },
+  tokensMap: {},
+};
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://kodahead.netlify.app",
   markdown: {
-    shikiConfig: {
-      theme: "material-theme-darker"
-    }
+    extendDefaultPlugins: true,
+    syntaxHighlight: false,
+    rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
   },
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), alpinejs(), partytown(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), mdx()]
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    alpinejs(),
+    partytown(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    mdx(),
+    sitemap(),
+  ],
 });
