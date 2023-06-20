e7858f9f15a38a64094d9aa942dd4a7dc3136915
Luka Gurdulic <phallondev@gmail.com>
2023-06-20 12:56:01 +0200

    Update dropdown colors and hover styling

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -40,11 +40,11 @@ const Search = ({ searchList }) => {
       />
 
       {posts.length > 0 && (
-        <ul className="bg-accent absolute top-[100%] right-0 p-[2px] rounded-lg menu gap-[1px] translate-y-4 z-50 shadow-xl md:max-w-[500px] max-w-full w-screen [&>*:last-child]:rounded-b-md [&>*:first-child]:rounded-t-md [&>*:only-child]:rounded-md">
+        <ul className="bg-base-300 border-2 border-accent absolute p-0 top-[100%] right-0 rounded-lg menu gap-[1px] translate-y-4 z-50 shadow-xl md:max-w-[500px] max-w-full w-screen [&>*:last-child]:rounded-b-md [&>*:first-child]:rounded-t-md [&>*:only-child]:rounded-md">
           {posts.map((post) => (
             <li key={post.id} className="overflow-hidden">
               <a
-                className="inline-flex w-full rounded-none gap-0 flex-col items-start bg-base-200 transition ease"
+                className="inline-flex w-full rounded-none gap-0 flex-col items-start bg-base-200 hover:bg-accent hover:text-white transition ease"
                 href={`/blog/${post.slug}`}>
                 <h3 className="font-semibold">{post.title}</h3>
                 <p className="text-sm my-2 line-clamp-2">{post.description}</p>
