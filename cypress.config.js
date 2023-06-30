c516793a475710e2db0588adae4f27e4f892bfd0
Luka Gurdulic <phallondev@gmail.com>
2023-06-30 18:06:46 +0200

    Config

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    supportFile: false,
+    supportFile: "cypress/e2e/support/index.{js,jsx,ts,tsx}",
     baseUrl: "http://localhost:3000",
     video: false,
     screenshotOnRunFailure: false,
+    watchForFileChanges: false,
+    runMode: "single",
   },
   env: {
     email: "phallondev@gmail.com",
