d1c418fe21345c59b566bdda726fb82f5a496526
Luka Gurdulic <phallondev@gmail.com>
2023-06-30 13:32:00 +0200

    Add character remaining counter test

--- cypress/e2e/contactPage/contactForm.cy.js
+++ cypress/e2e/contactPage/contactForm.cy.js
@@ -1,14 +1,25 @@
 it("should fill out the form, submit, and redirect to a page with reCAPTCHA", () => {
-  const email = Cypress.env("email"); // Retrieve the email environment variable
-  const formAction = `https://formsubmit.co/${email}`; // Form action URL
+  const email = Cypress.env("email");
+  const formAction = `https://formsubmit.co/${email}`;
 
   cy.visit("/contact");
+
+  // Fill out form
   cy.get("#name").type("John Doe", { scrollBehavior: "center" });
   cy.get("#email").type("john.doe@example.com", { scrollBehavior: "center" });
   cy.get("#message").type("Hello World", { scrollBehavior: "center" });
 
+  // Check if character counter is working
+  cy.get("span[data-charCounter]").should(($span) => {
+    const initialCount = 300;
+    const updatedCount = initialCount - "Hello World".length;
+    expect(updatedCount).to.equal(parseInt($span.text()));
+  });
+
+  // Change action attribute to local .env
   cy.get("form").invoke("attr", "action", formAction);
 
+  // Submit form
   cy.get("form").within(() => {
     cy.get('button[type="submit"]').click();
   });
